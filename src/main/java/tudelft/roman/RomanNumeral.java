a23acfee75c5834884a7e3cf551cb73ce9885892
Steve Freeman <sf105@m3p.co.uk>
2020-01-25 17:22:54 +0100

    Clean up RomanNumeral

--- src/main/java/tudelft/roman/RomanNumeral.java
+++ src/main/java/tudelft/roman/RomanNumeral.java
@@ -1,37 +1,21 @@
 package tudelft.roman;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class RomanNumeral {
+    private final static Map<Character, Integer> map = Map.of('I', 1, 'V', 5, 'X', 10, 'L', 50, 'C', 100, 'D', 500, 'M', 1000);
 
-    private static Map<Character, Integer> map;
+    public int convert(String roman) {
 
-    static {
-        map = new HashMap<Character, Integer>();
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
-    }
+        int arabic = 0;
+        for(int i = 0; i < roman.length(); i++) {
+            int currentNumber = map.get(roman.charAt(i));
+            int next = i+1 < roman.length() ? map.get(roman.charAt(i+1)) : 0;
 
-    public int convert(String s) {
-
-        int convertedNumber = 0;
-        for(int i = 0; i < s.length(); i++) {
-            int currentNumber = map.get(s.charAt(i));
-            int next = i+1 < s.length() ? map.get(s.charAt(i+1)) : 0;
-
-            if(currentNumber >= next)
-                convertedNumber += currentNumber;
-            else
-                convertedNumber -= currentNumber;
+            arabic += currentNumber >= next ? currentNumber : -currentNumber;
         }
 
-        return convertedNumber;
+        return arabic;
 
     }
 }
