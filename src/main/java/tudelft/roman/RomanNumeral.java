2deea3bafb2ebaa9139fa58060ea6eae26009d12
Steve Freeman <sf105@m3p.co.uk>
2020-01-27 18:08:57 +0100

    Simplify Romans by converting everything to digits first.

--- src/main/java/tudelft/roman/RomanNumeral.java
+++ src/main/java/tudelft/roman/RomanNumeral.java
@@ -6,15 +6,19 @@ public class RomanNumeral {
     private final static Map<Character, Integer> CHAR_TO_DIGIT = Map.of('I', 1, 'V', 5, 'X', 10, 'L', 50, 'C', 100, 'D', 500, 'M', 1000);
 
     public int asArabic(String roman) {
-        int arabic = 0;
-        for(int i = 0; i < roman.length(); i++) {
-            final var currentNumber = CHAR_TO_DIGIT.get(roman.charAt(i));
-            final var next = i+1 < roman.length() ? CHAR_TO_DIGIT.get(roman.charAt(i+1)) : 0;
+        final var digits = roman.chars().map(c -> CHAR_TO_DIGIT.get((char)c)).toArray();
 
-            arabic += currentNumber >= next ? currentNumber : -currentNumber;
+        int result = 0;
+        for(int i = 0; i < digits.length; i++) {
+            final var currentNumber = digits[i];
+            result += isSubtractive(digits, i, currentNumber) ? -currentNumber : currentNumber;
         }
 
-        return arabic;
+        return result;
+    }
 
+    private static boolean isSubtractive(int[] digits, int i, int currentNumber) {
+        return i + 1 < digits.length
+            && currentNumber < digits[i + 1];
     }
 }
