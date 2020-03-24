68f7db8737b46f0a6b324393be4a58c381cdcc17
Mauricio Aniche <mauricioaniche@gmail.com>
2020-03-24 17:09:00 +0100

    refactor

--- src/main/java/tudelft/christmas/DefaultClock.java
+++ src/main/java/tudelft/christmas/DefaultClock.java
@@ -3,9 +3,8 @@ package tudelft.christmas;
 import java.util.Calendar;
 
 public class DefaultClock implements Clock {
-
     @Override
-    public Calendar now () {
+    public Calendar now() {
         return Calendar.getInstance();
     }
 }
--- src/test/java/tudelft/christmas/ChristmasDiscountRefactoredTest.java
+++ src/test/java/tudelft/christmas/ChristmasDiscountRefactoredTest.java
@@ -1,35 +1,32 @@
 package tudelft.christmas;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 public class ChristmasDiscountRefactoredTest {
 
+    private final Clock clock = Mockito.mock(Clock.class);
+    private final ChristmasDiscountRefactored cd = new ChristmasDiscountRefactored(clock);
+
     @Test
     public void christmas() {
-        Clock clock = Mockito.mock(Clock.class);
         Calendar christmas = new GregorianCalendar(2015, Calendar.DECEMBER, 25);
+        when(clock.now()).thenReturn(christmas);
 
-        Mockito.when(clock.now()).thenReturn(christmas);
-
-        double finalValue = new ChristmasDiscountRefactored(clock).applyDiscount(100.0);
-
-        Assertions.assertEquals(85.0, finalValue, 0.0001);
+        double finalValue = cd.applyDiscount(100.0);
+        assertEquals(85.0, finalValue, 0.0001);
     }
 
     @Test
     public void notChristmas() {
-        Clock clock = Mockito.mock(Clock.class);
         Calendar christmas = new GregorianCalendar(2015, Calendar.JANUARY, 25);
+        when(clock.now()).thenReturn(christmas);
 
-        Mockito.when(clock.now()).thenReturn(christmas);
-
-        double finalValue = new ChristmasDiscountRefactored(clock).applyDiscount(100.0);
-
-        Assertions.assertEquals(100.0, finalValue, 0.0001);
+        double finalValue = cd.applyDiscount(100.0);
+        assertEquals(100.0, finalValue, 0.0001);
     }
 }
