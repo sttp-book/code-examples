1567f18aa459ce319ba5c6c7bcc7a63f95f04ce0
Frank Mulder <F.Mulder@tudelft.nl>
2020-03-30 19:01:37 +0200

    Implement asArabic using an iterator (and without streams)

--- src/main/java/tudelft/roman/RomanNumeral.java
+++ src/main/java/tudelft/roman/RomanNumeral.java
@@ -1,24 +1,27 @@
 package tudelft.roman;
 
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
 import java.util.Map;
 
 public class RomanNumeral {
-    private final static Map<Character, Integer> CHAR_TO_DIGIT = Map.of('I', 1, 'V', 5, 'X', 10, 'L', 50, 'C', 100, 'D', 500, 'M', 1000);
+    private final static Map<Character, Integer> RomanToArabic = 
+            Map.of('I', 1, 'V', 5, 'X', 10, 'L', 50, 'C', 100, 'D', 500, 'M', 1000);
 
     public int asArabic(String roman) {
-        final var digits = roman.chars().map(c -> CHAR_TO_DIGIT.get((char)c)).toArray();
+        final var iter = new StringCharacterIterator(roman);
 
+        var nextValue = 0;
         var result = 0;
-        for(int i = 0; i < digits.length; i++) {
-            final var currentNumber = digits[i];
-            result += isSubtractive(digits, i, currentNumber) ? -currentNumber : currentNumber;
+        for (char c = iter.last(); c != CharacterIterator.DONE; c = iter.previous()) {
+            var currentValue = RomanToArabic.get(c);
+            var isSubtractive = currentValue < nextValue;
+            var factor = isSubtractive ? -1 : 1;
+            result += factor * currentValue;
+
+            nextValue = currentValue;
         }
 
         return result;
-    }
-
-    private static boolean isSubtractive(int[] digits, int i, int currentNumber) {
-        return i + 1 < digits.length
-            && currentNumber < digits[i + 1];
     }
 }
