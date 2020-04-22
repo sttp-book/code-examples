5e3915b77e7164c1c9176804f016df8164f04bc7
Mauricio Aniche <mauricioaniche@gmail.com>
2020-04-22 22:00:36 +0200

    making tests to pass

--- src/test/java/tudelft/chocolate/ChocolateBarsTest4.java
+++ src/test/java/tudelft/chocolate/ChocolateBarsTest4.java
@@ -3,7 +3,6 @@ package tudelft.chocolate;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
