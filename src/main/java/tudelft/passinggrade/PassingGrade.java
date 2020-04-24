932b2fa082b2c0728f59d8108cc293df24977219
Evaldas Latoskinas <evaldasl61@gmail.com>
2020-04-24 15:20:57 +0200

    Implement PassingGrade exercise & corresponding tests

---
+++ src/main/java/tudelft/passinggrade/PassingGrade.java
@@ -0,0 +1,32 @@
+package tudelft.passinggrade;
+
+public class PassingGrade {
+    enum Concept {
+        A,
+        B,
+        C,
+        D,
+        E,
+        F
+    }
+
+    public Concept calculateGrade(float grade) {
+        if (grade < 1.0f || grade > 10.0f) {
+            throw new IllegalArgumentException("Invalid grade point specified!");
+        }
+
+        if (grade < 5.0f) {
+            return Concept.F;
+        } else if (grade < 6.0f) {
+            return Concept.E;
+        } else if (grade < 7.0f) {
+            return Concept.D;
+        } else if (grade < 8.0f) {
+            return Concept.C;
+        } else if (grade < 9.0f) {
+            return Concept.B;
+        } else {
+            return Concept.A;
+        }
+    }
+}
---
+++ src/test/java/tudelft/passinggrade/PassingGradeTest.java
@@ -0,0 +1,46 @@
+package tudelft.passinggrade;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test suite for calculating grades (the PassingGrade class)
+ */
+class PassingGradeTest {
+    PassingGrade passingGrade = new PassingGrade();
+
+    /**
+     * Parameterized test that tests lower & upper
+     * boundaries for all grade concepts.
+     *
+     * We consider lower boundaries which are inclusive, upper boundaries
+     * which are exclusive. We also use 2 test cases to test both sides
+     * of the exclusive boundaries.
+     *
+     * @param grade     Grade point (input argument)
+     * @param concept   Corresponding expected concept (enumeration)
+     */
+    @ParameterizedTest
+    @CsvSource({
+            "1.0f, F",  // Lower boundary (F)
+            "4.9f, F",  // In point (F)
+            "5.0f, E",  // Upper boundary (F), lower boundary (E)
+            "5.9f, E",  // In point (E)
+            "6.0f, D",  // Upper boundary (E), lower boundary (D)
+            "6.9f, D",  // In point (D)
+            "7.0f, C",  // Upper boundary (D), lower boundary (C)
+            "7.9f, C",  // In point (C)
+            "8.0f, B",  // Upper boundary (C), lower boundary (B)
+            "8.9f, B",  // In point (B)
+            "9.0f, A",  // Upper boundary (B), lower boundary (A)
+            "10.0f, A"  // Upper boundary (A)
+    })
+    public void testGradeBoundaryTests(float grade, PassingGrade.Concept concept) {
+        PassingGrade.Concept result = passingGrade.calculateGrade(grade);
+        assertEquals(concept, result, "Expected " + concept + " for grade " + grade);
+    }
+}
