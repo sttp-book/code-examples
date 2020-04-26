6b99fdb9e32780cec1558396217ffe7e65596f4b
Mauricio Aniche <mauricioaniche@gmail.com>
2020-04-26 23:17:05 +0200

    fix tests

--- src/test/java/tudelft/domain/TaxIncomeTest.java
+++ src/test/java/tudelft/domain/TaxIncomeTest.java
@@ -26,16 +26,16 @@ public class TaxIncomeTest {
 
     private static Stream<Arguments> generator() {
         return Stream.of(
-            Arguments.of(-1, TaxIncome.CANNOT_CALC_TAX),            // T1: OFF Point B1 lower
-            Arguments.of(0, 0),                                     // T2: ON Point B1 lower
-            Arguments.of(22099, 0.15 * 22099),                      // T3: OFF Point B1 upper & B2 lower
-            Arguments.of(22100, 3315),                              // T4: ON Point B1 upper & B2 lower
-            Arguments.of(53499, 3315 + 0.28 * (53499 - 22100)),     // T5: OFF Point B2 upper & B3 lower
-            Arguments.of(53500, 12107),                             // T6: ON Point B2 upper & B3 lower
-            Arguments.of(114999, 12107 + 0.31 * (114999 - 53500)),  // T7: OFF Point B3 upper & B4 lower
-            Arguments.of(115000, 31172),                            // T8: ON Point B3 upper & B4 lower
-            Arguments.of(249999, 31172 + 0.36 * (249999 - 115000)), // T9: OFF Point B4 upper & B5 lower
-            Arguments.of(250000, 79772)                             // T10: ON Point B4 upper & B5 lower
+            Arguments.of(-1.0, TaxIncome.CANNOT_CALC_TAX),            // T1: OFF Point B1 lower
+            Arguments.of(0.0, 0.0),                                     // T2: ON Point B1 lower
+            Arguments.of(22099.0, 0.15 * 22099),                      // T3: OFF Point B1 upper & B2 lower
+            Arguments.of(22100.0, 3315.0),                              // T4: ON Point B1 upper & B2 lower
+            Arguments.of(53499.0, 3315.0 + 0.28 * (53499 - 22100)),     // T5: OFF Point B2 upper & B3 lower
+            Arguments.of(53500.0, 12107.0),                             // T6: ON Point B2 upper & B3 lower
+            Arguments.of(114999.0, 12107.0 + 0.31 * (114999 - 53500)),  // T7: OFF Point B3 upper & B4 lower
+            Arguments.of(115000.0, 31172.0),                            // T8: ON Point B3 upper & B4 lower
+            Arguments.of(249999.0, 31172.0 + 0.36 * (249999 - 115000)), // T9: OFF Point B4 upper & B5 lower
+            Arguments.of(250000.0, 79772.0)                             // T10: ON Point B4 upper & B5 lower
         );
 
     }
