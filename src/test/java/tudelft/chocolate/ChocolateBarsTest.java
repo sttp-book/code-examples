c8ed9a3c547913b24105d000d89c97bbe3223aed
Mauricio Aniche <mauricioaniche@gmail.com>
2020-04-27 13:06:55 +0200

    just one chocolate bar code

--- src/test/java/tudelft/chocolate/ChocolateBarsTest.java
+++ src/test/java/tudelft/chocolate/ChocolateBarsTest.java
@@ -2,37 +2,41 @@ package tudelft.chocolate;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 public class ChocolateBarsTest {
-    @ParameterizedTest(name = "small={0}, big={1}, total={2}, result={3}")
-    @CsvSource({ "1,1,5,0", "1,1,6,1", "1,1,7,-1", "1,1,8,-1" })
-    void totalIsTooBig(int small, int big, int total, int expectedResult) {
-        ChocolateBars bags = new ChocolateBars();
-        int result = bags.calculate(small, big, total);
-        Assertions.assertEquals(expectedResult, result);
-    }
 
-    @ParameterizedTest(name = "small={0}, big={1}, total={2}, result={3}")
-    @CsvSource({ "4,0,10,-1", "4,1,10,-1", "5,2,10,0", "5,3,10,0" })
-    void onlyBigBars(int small, int big, int total, int expectedResult) {
+    @ParameterizedTest
+    @MethodSource("generator")
+    void boundaries(int small, int big, int total, int expectedResult) {
         int result = new ChocolateBars().calculate(small, big, total);
         Assertions.assertEquals(expectedResult, result);
     }
 
-    @ParameterizedTest(name = "small={0}, big={1}, total={2}, result={3}")
-    @CsvSource({
-        "0,3,17,-1", "1,3,17,-1", "2,3,17,2", "3,3,17,2",
-        "0,3,12,-1", "1,3,12,-1", "2,3,12,2", "3,3,12,2"})
-    void bigAndSmallBars(int small, int big, int total, int expectedResult) {
-        int result = new ChocolateBars().calculate(small, big, total);
-        Assertions.assertEquals(expectedResult, result);
+    private static Stream<Arguments> generator() {
+        return Stream.of(
+                // small and big bars -> only big bars
+                Arguments.of(10,1,10,5),
+                Arguments.of(10,2,10,0),
+                // small and big bars -> only small bars
+                Arguments.of(10,1,10,5),
+                Arguments.of(10,0,10,10),
+                // only small bars -> not enough bars
+                Arguments.of(5,0,5,5),
+                Arguments.of(4,0,5,-1),
+                // only big bars -> not enough bars
+                Arguments.of(4,2,10,0),
+                Arguments.of(4,1,10,-1),
+                // small and big bars -> not enough bars
+                Arguments.of(3,1,8,3),
+                Arguments.of(2,1,8,-1),
+                Arguments.of(3,1,8,3),
+                Arguments.of(3,0,8,-1)
+        );
+
     }
 
-    @ParameterizedTest(name = "small={0}, big={1}, total={2}, result={3}")
-    @CsvSource({ "4,2,3,3", "3,2,3,3", "2,2,3,-1", "1,2,3,-1" })
-    void onlySmallBars(int small, int big, int total, int expectedResult) {
-        int result = new ChocolateBars().calculate(small, big, total);
-        Assertions.assertEquals(expectedResult, result);
-    }
 }
