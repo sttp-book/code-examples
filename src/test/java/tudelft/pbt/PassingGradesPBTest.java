09f9af82f5853e2904dc48436c1dc59b92ac4f6a
Mauricio Aniche <mauricioaniche@gmail.com>
2020-04-27 16:15:30 +0200

    one more PBT

---
+++ src/test/java/tudelft/pbt/PassingGradesPBTest.java
@@ -0,0 +1,38 @@
+package tudelft.pbt;
+
+import net.jqwik.api.*;
+import net.jqwik.api.constraints.FloatRange;
+import tudelft.domain.PassingGrade;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class PassingGradesPBTest {
+
+    private final PassingGrade pg = new PassingGrade();
+
+    @Property
+    void pass(@ForAll @FloatRange(min = 1f, max = 5.0f, maxIncluded = false) float grade) {
+        assertThat(pg.passed(grade)).isFalse();
+    }
+
+    @Property
+    void fail(@ForAll @FloatRange(min = 5.0f, max = 10.0f, maxIncluded = false) float grade) {
+        assertThat(pg.passed(grade)).isTrue();
+    }
+
+    @Property
+    void invalid(@ForAll("invalidGrades") float grade) {
+        assertThatThrownBy(() -> {
+            pg.passed(grade);
+        })
+        .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Provide
+    private Arbitrary<Float> invalidGrades() {
+        return Arbitraries.oneOf(
+                Arbitraries.floats().lessOrEqual(-0.9f),
+                Arbitraries.floats().greaterOrEqual(10.1f));
+    }
+}
